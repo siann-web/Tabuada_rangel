15e476e6bcc21701e114810566a1a02df7703892
Ruan Bruno <81209795+siann-web@users.noreply.github.com>
2024-04-16 08:02:57 -0300

    Atualização dia 16/04/2024

---
+++ Trabalho Tabuada Rangel Rev2/JS/script.js
@@ -0,0 +1,73 @@
+import {rbs_numbers, rbs_operations, rbs_result, rbs_clear, rbs_display, rbs_deleteLast, rbs_DisplayResult} from './QuerySelector.js';
+
+// rbs é a assinatura do meu nome, Ruan Bruno Silva.
+
+rbs_numbers.forEach((number) => {
+    number.addEventListener("click", () => {
+        if(rbs_display.textContent === "0"){
+            rbs_display.textContent = "";
+        }
+        rbs_display.textContent += number.textContent;
+    });
+});    
+;
+  rbs_operations.forEach((op) => {
+    op.addEventListener("click", () => {
+        rbs_display.textContent += op.textContent;
+    }) 
+});
+
+rbs_result.addEventListener("click", () => {
+    let rbs_tabuada = "";
+    
+    let rbs_operationIndice = rbs_display.textContent.indexOf('+');
+    if(rbs_operationIndice !== -1){
+        let rbs_number = parseInt(rbs_display.textContent.slice(0, rbs_operationIndice));
+        for(let i = 1; i <= 10; i++){
+            rbs_tabuada += `${parseFloat(rbs_number)} + ${i} = ${rbs_number + i}<br>`;
+            rbs_DisplayResult.innerHTML = rbs_tabuada;
+        }
+    }
+
+    rbs_operationIndice = rbs_display.textContent.indexOf('-');
+    if(rbs_operationIndice !== -1){
+        let rbs_number = parseInt(rbs_display.textContent.slice(0, rbs_operationIndice));
+        for(let i = 1; i <= 10; i++){
+            rbs_tabuada += `${parseFloat(rbs_number)} - ${i} = ${rbs_number - i}<br>`;
+            rbs_DisplayResult.innerHTML = rbs_tabuada;
+        }
+    }
+
+    rbs_operationIndice = rbs_display.textContent.indexOf('*');
+    if(rbs_operationIndice !== -1){
+        let rbs_number = parseInt(rbs_display.textContent.slice(0, rbs_operationIndice));
+        for(let i = 1; i <= 10; i++){
+            rbs_tabuada += `${parseFloat(rbs_number)} * ${i} = ${rbs_number * i}<br>`;
+            rbs_DisplayResult.innerHTML = rbs_tabuada;
+        }
+    }
+
+    rbs_operationIndice = rbs_display.textContent.indexOf('/');
+    if(rbs_operationIndice !== -1){
+        let rbs_number = parseInt(rbs_display.textContent.slice(0, rbs_operationIndice));
+        for(let i = 1; i <= 10; i++){
+            rbs_tabuada += `${parseFloat(rbs_number)} / ${i} = ${rbs_number / i}<br>`;
+            rbs_DisplayResult.innerHTML = rbs_tabuada;
+        }
+    }
+});
+
+rbs_clear.addEventListener("click", () => {
+    if(rbs_display.textContent !== ""){
+        rbs_display.textContent = "";
+        rbs_DisplayResult.innerHTML = "0";
+    }
+
+});
+
+rbs_deleteLast.addEventListener("click", () => {
+    if(rbs_display.textContent !== ""){
+        let rbs_clearOne = rbs_display.textContent.length;
+        rbs_display.textContent = rbs_display.textContent.slice(0, rbs_clearOne - 1);
+    }
+}); 
