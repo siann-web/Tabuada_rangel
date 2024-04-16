1f5056368cf2c415418850dcb2314042c328495e
Ruan Bruno <81209795+siann-web@users.noreply.github.com>
2024-04-16 07:59:37 -0300

    Delete Trabalho Tabuada Rangel Rev2 directory

--- Trabalho Tabuada Rangel Rev2/JS/QuerySelector.js
+++
@@ -1,7 +0,0 @@
-export var rbs_numbers = document.querySelectorAll(".top div, .bot div, .mid div");
-export var rbs_operations = document.querySelectorAll(".topp div");
-export var rbs_result = document.querySelector("#result");
-export var rbs_deleteLast = document.querySelector("#delete");
-export var rbs_clear = document.querySelector("#clear");
-export var rbs_display = document.querySelector(".display");
-export var rbs_DisplayResult = document.querySelector(".displayResult");
